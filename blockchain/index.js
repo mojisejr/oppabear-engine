bc9f6d2eedb4b0cd45e646c862b3c7cfd99ece0a
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2023-02-27 20:03:52 +0700

    update: contract addresses

--- blockchain/index.js
+++ blockchain/index.js
@@ -8,10 +8,10 @@ const labAbi = require("./labs.abi");
 const config = {
   providerUrl:
     "https://opt-goerli.g.alchemy.com/v2/FJoUwTBR1cpQiyK2gzrELLoP1ZsAmatQ",
-  hostAddr: "0xEBcEF7c7cb28eC904Eb69E9327cE0a465BB68A49",
-  mutantAddr: "0xC08efD523a3F1a238a09Bf84a16725de6725d9F5",
-  stimulusAddr: "0x74c68931B655636EC1C438616E3eB59231133D2E",
-  labAddr: "0x8b3Ea677f28DCF6032279885DFbeBe577C2e0bC5",
+  hostAddr: "0x2873eD15d349B3eCD048B6036Dcab89E87Aeb4F8",
+  mutantAddr: "0xb90C4fD94f7E96ED04141AD0f53256D4A51178A3",
+  stimulusAddr: "0x13a5631D92DD04E1fF802FEF0e70b392B660a86F",
+  labAddr: "0x8D342304292e28696Ce10bA15cc88E255C612734",
 };
 
 const provider = new ethers.providers.JsonRpcProvider(config.providerUrl);
